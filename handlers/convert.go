ccdc4e17c990696f9d70997e6a2aad7c386510df
Jeffrey Ting <jeffrey_ting@cargill.com>
2026-02-05 02:17:15 -0500

    fix heic check

--- handlers/convert.go
+++ handlers/convert.go
@@ -1,15 +1,17 @@
 package handlers
 
 import (
+	"bufio"
 	"encoding/json"
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
-	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	"github.com/dolmen-go/kittyimg"
 	"github.com/kingtingthegreat/img2lego/converter"
 	"github.com/kingtingthegreat/img2lego/util"
 	"github.com/nfnt/resize"
@@ -21,31 +23,26 @@ const (
 	defaultWidth  = 100
 )
 
-func IsAppleHEIC(r io.Reader) (bool, error) {
-	// HEIC brand is within first 12 bytes
-	buf := make([]byte, 12)
-	_, err := io.ReadFull(r, buf)
-	if err != nil {
-		return false, err
+func IsAppleHEICHeader(peek []byte) bool {
+	if len(peek) < 12 {
+		return false
 	}
-
-	// bytes 4–7 must be "ftyp"
-	if string(buf[4:8]) != "ftyp" {
-		return false, nil
+	if string(peek[4:8]) != "ftyp" {
+		return false
 	}
-
-	brand := string(buf[8:12])
-
-	switch brand {
+	switch string(peek[8:12]) {
 	case "heic", "heix", "hevc", "hevx", "mif1", "msf1":
-		return true, nil
+		return true
 	default:
-		return false, nil
+		return false
 	}
 }
 
 func ConvertHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
+
+	br := bufio.NewReader(r.Body)
+	peek, _ := br.Peek(12)
 
 	var width uint
 	qWidth, err := strconv.Atoi(r.URL.Query().Get("width"))
@@ -65,9 +62,9 @@ func ConvertHandler(w http.ResponseWriter, r *http.Request) {
 	// r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
 
 	// raw image in body
-	img, _, err := image.Decode(r.Body)
+	img, _, err := image.Decode(br)
 	if err != nil {
-		log.Println(err)
+		log.Println("error decoding image", err)
 		http.Error(w, "failed to read body: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -76,14 +73,10 @@ func ConvertHandler(w http.ResponseWriter, r *http.Request) {
 
 	// TODO clean up this heic check
 	// check if image is heic
-	heic, err := IsAppleHEIC(r.Body)
-	if err != nil {
-		log.Println(err)
-		heic = false
-	}
-
+	heic := IsAppleHEICHeader(peek)
 	// if heic, rotate 90 degrees clockwise
 	if heic {
+		log.Println("Rotating HEIC image")
 		b := img.Bounds()
 		w, h := b.Dx(), b.Dy()
 
@@ -94,6 +87,9 @@ func ConvertHandler(w http.ResponseWriter, r *http.Request) {
 				dst.Set(h-1-y, x, img.At(b.Min.X+x, b.Min.Y+y))
 			}
 		}
+
+		kittyimg.Fprintln(os.Stdout, dst) // to avoid memory leak in kittyimg
+		kittyimg.Fprintln(os.Stdout, img) // to avoid memory leak in kittyimg
 
 		img = dst
 	}
