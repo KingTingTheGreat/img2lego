6d82f252e25e0335acea32106dd36c8c71437c9f
Jeffrey Ting <jeffrey_ting@cargill.com>
2026-02-05 01:40:46 -0500

    fix cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,10 +2,20 @@ package middleware
 
 import "net/http"
 
+var allowedOrigins map[string]bool = map[string]bool{
+	"http://localhost:3000":           true,
+	"https://img2lego-web.vercel.app": true,
+	"https://img2lego.jting.org":      true,
+}
+
 // net/http example
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000,https://img2lego-web.vercel.app,https://img2lego.jting.org")
+		origin := r.Header.Get("Origin")
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Vary", "Origin") // important for caches
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
